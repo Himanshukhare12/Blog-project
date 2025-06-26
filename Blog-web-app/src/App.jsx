6147dbefb707e238a5478ada1f0df6cd4962c6ab
Himanshu khare <hkhare666@gmail.com>
2025-06-26 09:23:07 +0530

    Adding authentication service using app write

---
+++ Blog-web-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <div>Welcome to my blog project with appwrite</div>
+    </>
+  )
+}
+
+export default App
