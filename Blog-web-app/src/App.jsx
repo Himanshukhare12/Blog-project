1b800993e6f213891d0ae0c514425b422843323c
Himanshu khare <hkhare666@gmail.com>
2025-06-27 08:08:27 +0530

    Adding Databases and Storage Services

--- Blog-web-app/src/App.jsx
+++ Blog-web-app/src/App.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css'
-
+import {useDispatch} from 'react-redux'
+import authService from './Appwrite/auth';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <div>Welcome to my blog project with appwrite</div>
-    </>
-  )
+  const [loading, setLoading] = useState(true)  
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser().then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+
+    }).finally(()=>{setLoading(false)})
+  },[])
+    return loading?null:(<div className='min-h-screen flex flex-wrap
+    content-between bg-gray-400'>
+      <div className='w-full block'>
+      <Header />
+      <main>
+        {/* <Outlet /> */}
+      </main>
+      <Footer /> 
+      </div></div>)
 }
 
 export default App
